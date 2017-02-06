3aa16ed100ad9455882ba68aa919158a94d162da
Hosana <Hosana@10.1.27.77>
2017-02-06 17:05:26 -0200

    Criação de esqueleto do teste unitário de Grupo. (Sem programação)

---
+++ aacomp3/testes/testes/GrupoTeste.java
@@ -0,0 +1,39 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GrupoTeste {
+
+	@Test
+	public void testGrupoUsuarioStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGrupoUsuarioStringStringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionarUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAtivo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDesativar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArmazenar() {
+		fail("Not yet implemented");
+	}
+
+}
